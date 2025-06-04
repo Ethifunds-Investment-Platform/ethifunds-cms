46eb90c54c4fde7aabc43d283a01715e0e249b9e
sylvahh <sylvesterchambers03@gmail.com>
2025-06-04 03:22:21 +0100

    chore: made some fixes

--- src/features/savings/savings-details/savings-info/active-contributors-list/contributor-card.tsx
+++ src/features/savings/savings-details/savings-info/active-contributors-list/contributor-card.tsx
@@ -5,7 +5,7 @@ import { SavingsContributor } from "@/types/savings.types";
 export default function ContributorCard(props: SavingsContributor) {
 	return (
 		<div className="flex justify-between [&_span]:content-standard capitalize">
-			<span> {props.username} </span>
+			<span> {props.username.toLowerCase()} </span>
 			<span>
 				{" "}
 				{amountSeparator(props.amount_raised)}/{amountSeparator(props.target_amount)}{" "}
