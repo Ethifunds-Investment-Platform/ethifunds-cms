f9abafe947d30d77216082ee2db9d4154077c341
sylvahh <sylvesterchambers03@gmail.com>
2025-07-11 21:24:58 +0100

    feat: add Metrics component to Savings feature

--- src/features/savings/index.tsx
+++ src/features/savings/index.tsx
@@ -2,14 +2,15 @@ import AppContainer from "@/components/container/container";
 import CreateSavingsButton from "./create-savings-button";
 import useUi from "@/hooks/use-ui";
 import SavingsWithdrawals from "./savings-withdrawals";
+import Metrics from "./metrics";
 
 export default function Savings() {
 	useUi({ title: "Savings" });
 
 	return (
 		<AppContainer className="relative space-y-20">
+			<Metrics />
 			<CreateSavingsButton />
-
 			<SavingsWithdrawals />
 		</AppContainer>
 	);
